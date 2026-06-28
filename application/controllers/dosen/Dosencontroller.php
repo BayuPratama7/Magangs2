e9ff38fa315e98c1062b73e520ea0c84305e2cd2
Admin Magang <admin@magang.local>
2026-06-28 13:10:00 +0700

    fix(controllers): fix case sensitivity for CodeIgniter 3 routing on Linux

--- application/controllers/dosen/Dosencontroller.php
+++ application/controllers/dosen/Dosencontroller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DosenController extends CI_Controller
+class Dosencontroller extends CI_Controller
 {
 
     public function __construct()
